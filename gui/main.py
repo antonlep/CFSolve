c026f2ab05ed5483dadedad9dfb8c300433718f8
antonlep <antonleppa@gmail.com>
2025-11-30 21:18:17 +0200

    gui signals

--- gui/main.py
+++ gui/main.py
@@ -1,23 +1,50 @@
 import sys
 
 from PySide6.QtCore import QSize
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QLineEdit,
+    QMainWindow,
+    QVBoxLayout,
+    QWidget,
+)
 
 import solver
+
+window_titles = [
+    "My App",
+    "My App",
+    "Still My App",
+    "Still My App",
+    "What on earth",
+    "What on earth",
+    "This is surprising",
+    "This is surprising",
+    "Something went wrong",
+]
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
-        button.setCheckable(True)
-        button.clicked.connect(self.the_button_was_clicked)
-        self.setFixedSize(QSize(400, 300))
-        self.setCentralWidget(button)
 
-    def the_button_was_clicked(self):
-        print("Clicked!")
+        self.setWindowTitle("My App")
+
+        self.label = QLabel()
+
+        self.input = QLineEdit()
+        self.input.textChanged.connect(self.label.setText)
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.input)
+        layout.addWidget(self.label)
+
+        container = QWidget()
+        container.setLayout(layout)
+
+        self.setFixedSize(QSize(400, 300))
+        self.setCentralWidget(container)
 
 
 app = QApplication(sys.argv)
